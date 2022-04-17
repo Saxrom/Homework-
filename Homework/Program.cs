c08f1fc3d5bc2d17e74327d5f591e4e09e94976e
Shahrom <shahromkr@code.edu.az>
2022-04-18 01:22:26 +0400

    added homework

---
+++ Homework/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Eded daxil edin");                   \\Task 2
+            //int Dividend = Convert.ToInt32(Console.ReadLine());
+            //int reminder = default(int);
+            //int minus = default(int);
+
+
+            //if (Dividend % 7 == 0)
+            //{
+            //    Console.WriteLine("7-ye bolunur");
+            //}
+            //else if (Dividend % 7 != 0)
+            //{
+            //    reminder = Dividend % 7;
+            //    minus = Dividend - reminder;
+            //    {
+            //        Console.WriteLine(minus);
+            //    }
+            //}  
+
+
+            //Numberplace(24, 59);                                     \\Task 1
+
+
+            //int number = Convert.ToInt32(Console.ReadLine());\\      \\Task 3
+            //Console.WriteLine(FindArray(number));
+
+        }
+
+
+
+        //public static void Numberplace(int number, int a)
+        //{
+        //    int count = 0;
+        //    int newnomber = number;
+        //    while (newnomber > 0)
+        //    {
+        //        count++;
+        //        newnomber /= 10;
+        //    }
+        //    int quantity = 1;
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        quantity *= 10;
+        //    }
+        //    number = number + a * quantity;
+        //    Console.WriteLine(number);
+        //}
+
+
+        //public static int FindArray(int num)
+        //{
+
+        //    for (int i = 0; i < num; i++)
+        //    {
+        //        if (i < 50 && i % 3 == 0)
+        //        {
+        //            Console.WriteLine(i);
+
+        //        }
+
+        //        if (i > 50 && i < 100 && i % 5 == 0)
+        //        {
+        //            Console.WriteLine(i);
+
+        //        }
+
+        //        if (i > 100 && i % 8 == 0)
+        //        {
+        //            Console.WriteLine(i);
+
+        //        }
+        //    }
+        //    return num;
+
+        //}
+
+
+    }
+}
